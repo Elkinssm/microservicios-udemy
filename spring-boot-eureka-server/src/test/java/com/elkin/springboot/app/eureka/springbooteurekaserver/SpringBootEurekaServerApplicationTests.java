2781d08f793b960d92ab562db75f424fb4d3d102
elkin.silva <elkinsilvamanrique@gmail.com>
2023-03-15 16:16:09 -0500

    Add eureka server

---
+++ spring-boot-eureka-server/src/test/java/com/elkin/springboot/app/eureka/springbooteurekaserver/SpringBootEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elkin.springboot.app.eureka.springbooteurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
