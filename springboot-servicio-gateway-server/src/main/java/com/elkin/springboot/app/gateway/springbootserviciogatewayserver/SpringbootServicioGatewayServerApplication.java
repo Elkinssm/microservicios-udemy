206ec0ec299d785fc1cdd8c79be7b85b36ba540e
elkin.silva <elkinsilvamanrique@gmail.com>
2023-03-16 11:18:54 -0500

    Add gateway

---
+++ springboot-servicio-gateway-server/src/main/java/com/elkin/springboot/app/gateway/springbootserviciogatewayserver/SpringbootServicioGatewayServerApplication.java
@@ -0,0 +1,15 @@
+package com.elkin.springboot.app.gateway.springbootserviciogatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringbootServicioGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioGatewayServerApplication.class, args);
+	}
+
+}
