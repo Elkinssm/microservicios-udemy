3a869e30008fd60181cdd13b324d2acd8bcfabef
elkin.silva <elkinsilvamanrique@gmail.com>
2023-03-17 12:00:16 -0500

    Add circuitbraker

--- springboot-servicio-item/src/main/java/com/elkin/springbootservicioitem/controller/ItemController.java
+++ springboot-servicio-item/src/main/java/com/elkin/springbootservicioitem/controller/ItemController.java
@@ -1,15 +1,24 @@
 package com.elkin.springbootservicioitem.controller;
 
 import com.elkin.springbootservicioitem.models.Item;
+import com.elkin.springbootservicioitem.models.Producto;
 import com.elkin.springbootservicioitem.models.service.ItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class ItemController {
+
+    private final Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+    @Autowired
+    private CircuitBreakerFactory cbFactory;
 
     @Autowired
     @Qualifier("serviceFeign")
@@ -24,6 +33,20 @@ public class ItemController {
 
     @GetMapping("/ver/{id}/cantidad/{cantidad}")
     public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
-        return itemService.findById(id, cantidad);
+//        return itemService.findById(id, cantidad);
+        return cbFactory.create("items")
+                .run(() -> itemService.findById(id, cantidad)/*, e -> metodoAlternativo(id, cantidad, e)*/);
+    }
+
+    public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
+        logger.info(e.getMessage());
+        Item item = new Item();
+        Producto producto = new Producto();
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Camara Sony");
+        producto.setPrecio(500.00);
+        item.setProducto(producto);
+        return item;
     }
 }
--- springboot-servicios-productos/src/main/java/com/elkin/springbootserviciosproductos/controller/ProductoController.java
+++ springboot-servicios-productos/src/main/java/com/elkin/springbootserviciosproductos/controller/ProductoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @RestController
@@ -19,14 +20,14 @@ public class ProductoController {
     private Environment env;
 
     @Value("${server.port}")
-    private  Integer port;
+    private Integer port;
 
     @Autowired
     private IProductoService productoService;
 
     @GetMapping("/listar")
-    public List<Producto> listar(){
-        return productoService.findAll().stream().map(producto ->{
+    public List<Producto> listar() {
+        return productoService.findAll().stream().map(producto -> {
             producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
             //producto.setPort(port);
             return producto;
@@ -34,19 +35,17 @@ public class ProductoController {
     }
 
     @GetMapping("/ver/{id}")
-    public Producto detalle(@PathVariable Long id) {
+    public Producto detalle(@PathVariable Long id) throws InterruptedException {
+        if (id.equals(10)) {
+            throw new IllegalStateException("Producto no encontrado");
+        }
+
+        if (id.equals(7L)) {
+            TimeUnit.SECONDS.sleep(5);
+        }
         Producto producto = productoService.findById(id);
         producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
         //producto.setPort(port);
-
-		/*
-		 * try {
-			Thread.sleep(2000L);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-
         return producto;
     }
 
