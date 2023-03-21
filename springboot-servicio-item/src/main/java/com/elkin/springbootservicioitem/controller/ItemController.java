9991ee6aaee2abffd35f3a0e9a5d5e2317392cd8
elkin.silva <elkinsilvamanrique@gmail.com>
2023-03-21 14:57:59 -0500

    Finis circuitbreaker unit

--- springboot-servicio-item/src/main/java/com/elkin/springbootservicioitem/controller/ItemController.java
+++ springboot-servicio-item/src/main/java/com/elkin/springbootservicioitem/controller/ItemController.java
@@ -3,6 +3,8 @@ package com.elkin.springbootservicioitem.controller;
 import com.elkin.springbootservicioitem.models.Item;
 import com.elkin.springbootservicioitem.models.Producto;
 import com.elkin.springbootservicioitem.models.service.ItemService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class ItemController {
@@ -38,6 +41,21 @@ public class ItemController {
                 .run(() -> itemService.findById(id, cantidad), e -> metodoAlternativo(id, cantidad, e));
     }
 
+    @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
+    @GetMapping("/ver2/{id}/cantidad/{cantidad}")
+    public Item detalle2(@PathVariable Long id, @PathVariable Integer cantidad) {
+        return itemService.findById(id, cantidad);
+
+    }
+
+    @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo2")
+    @TimeLimiter(name = "items")
+    @GetMapping("/ver3/{id}/cantidad/{cantidad}")
+    public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {
+        return CompletableFuture.supplyAsync(() -> itemService.findById(id, cantidad));
+
+    }
+
     public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
         logger.info(e.getMessage());
         Item item = new Item();
@@ -48,5 +66,17 @@ public class ItemController {
         producto.setPrecio(500.00);
         item.setProducto(producto);
         return item;
+    }
+
+    public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e) {
+        logger.info(e.getMessage());
+        Item item = new Item();
+        Producto producto = new Producto();
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Camara Sony");
+        producto.setPrecio(500.00);
+        item.setProducto(producto);
+        return CompletableFuture.supplyAsync(() -> item);
     }
 }
