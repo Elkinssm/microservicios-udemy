9b393c0ba2937e06fa9df82ff1825f76372c4e60
elkin.silva <elkinsilvamanrique@gmail.com>
2023-03-23 10:41:24 -0500

    Add config server

--- springboot-servicio-item/src/main/java/com/elkin/springbootservicioitem/controller/ItemController.java
+++ springboot-servicio-item/src/main/java/com/elkin/springbootservicioitem/controller/ItemController.java
@@ -9,10 +9,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -22,6 +27,9 @@ public class ItemController {
 
     @Autowired
     private CircuitBreakerFactory cbFactory;
+
+    @Value("${configuracion.texto}")
+    private String texto;
 
     @Autowired
     @Qualifier("serviceFeign")
@@ -37,8 +45,7 @@ public class ItemController {
     @GetMapping("/ver/{id}/cantidad/{cantidad}")
     public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 //        return itemService.findById(id, cantidad);
-        return cbFactory.create("items")
-                .run(() -> itemService.findById(id, cantidad), e -> metodoAlternativo(id, cantidad, e));
+        return cbFactory.create("items").run(() -> itemService.findById(id, cantidad), e -> metodoAlternativo(id, cantidad, e));
     }
 
     @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
@@ -78,5 +85,16 @@ public class ItemController {
         producto.setPrecio(500.00);
         item.setProducto(producto);
         return CompletableFuture.supplyAsync(() -> item);
+    }
+
+    @GetMapping("/obtener-config")
+    public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+
+        logger.info(texto);
+
+        Map<String, String> json = new HashMap<>();
+        json.put("texto", texto);
+        json.put("puerto", puerto);
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
     }
 }
